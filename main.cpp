981622d88cbd88ea9bda6128b6989f3c83be920d
Duncan Sterken <duncan.sterken123@gmail.com>
2018-12-11 14:09:58 +0100

    Add an interactive menu

--- main.cpp
+++ main.cpp
@@ -4,8 +4,30 @@
 #include "lessons/bmi.h"
 
 int main() {
-    tip();
-    temperature();
-    bmi();
-    return 0;
+
+    std::string choice;
+
+    std::cout << "Choose an action (tip, temp, bmi): ";
+    std::cin >> choice;
+
+    if (choice == "tip") {
+        tip();
+
+        return 0;
+    }
+
+    if (choice == "temp") {
+        temperature();
+
+        return 0;
+    }
+
+    if (choice == "bmi") {
+        bmi();
+
+        return 0;
+    }
+
+    std::cout << "Invalid item\n";
+    return 1;
 }
